0f7e6267909028fb21fb3c1b04da00a64aeda977
Leandro Gaspar Scanavini <leandrogaspar789@gmail.com>
2019-01-15 21:09:16 -0200

    Selenium flow does not require restart anymore

--- src/worker-server/default-nodes/create-driver.ts
+++ src/worker-server/default-nodes/create-driver.ts
@@ -5,12 +5,14 @@ import * as chromedriver from "chromedriver";
 import { IContext } from "../core/interfaces";
 import { Browsers, CreateDriverConfig } from "../../common";
 
-export async function createDriver(context: IContext, nodeConfig: CreateDriverConfig) {
+// This shouldn't be executed everytime the node is callled
+// Not sure if it is the best place but will be here for now
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 
+export async function createDriver(context: IContext, nodeConfig: CreateDriverConfig) {
     console.log(`Creating selenium driver ${nodeConfig.config.driverName} browser ${nodeConfig.config.browser}`);
 
     if (nodeConfig.config.browser === Browsers.CHROME) {
-        chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
         const driver = await new Builder().forBrowser('chrome').build();
         context.set(nodeConfig.config.driverName, driver);
 
